cd08c51120a2da7981c8caa5927321bdaacf08f8
vicSahmtech <m.ahmad@sahmtech.com>
2023-10-04 15:10:38 -0800

    fixed 500

--- Modules/Essentials/Http/Controllers/DataController.php
+++ Modules/Essentials/Http/Controllers/DataController.php
@@ -467,32 +467,21 @@ class DataController extends Controller
 
                         )->order(9);
 
-                        if (auth()->user()->can('curd_organizational_structure')) 
-                        {
-                            $subMenu->url(
+                            if (auth()->user()->can('curd_organizational_structure')) 
+                            {
+                                $subMenu->url(
 
-                                action([\Modules\Essentials\Http\Controllers\EssentialsDepartmentsController::class, 'index']),
-                                        __('essentials::lang.organizational_structure'),
-                                     ['icon' => 'fa fas fa-plus-circle', 'active' => request()->segment(1) == 'hrm' && request()->segment(2) == 'settings'],
-        
-        //                             action([\Modules\Essentials\Http\Controllers\EssentialsSettingsController::class, 'edit']),
-        //                             __('essentials::lang.organizational_structure'),
-                                 
-        
-                                )->order(10);
-                        }
+                                    action([\Modules\Essentials\Http\Controllers\EssentialsDepartmentsController::class, 'index']),
+                                            __('essentials::lang.organizational_structure'),
+                                        ['icon' => 'fa fas fa-plus-circle', 'active' => request()->segment(1) == 'hrm' && request()->segment(2) == 'settings'],
+            
+                                    
+            
+                                    )->order(10);}
+                       
+
+                        }},
                         
-
-                     /*   )->order(9);}
-                        
-                        $subMenu->url(
-                            action([\Modules\Essentials\Http\Controllers\EssentialsDepartmentsController::class, 'index']),
-                                __('essentials::lang.organizational_structure'),
-                             ['icon' => 'fa fas fa-plus-circle', 'active' => request()->segment(1) == 'hrm' && request()->segment(2) == 'settings'],                     
-
-                        )->order(10);*/
-
-                    },
                     [
                         'icon' => 'fa fas fa-users',
                         'active' => request()->segment(1) == 'essentials',
