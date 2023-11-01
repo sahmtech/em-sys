7ca5c2b3c45bf1b0f61264e8d59df91fd3e4c66b
Rama Abbas <r.abbas@sahmtech.com>
2023-11-01 09:28:59 +0200

    index of hosingmovement

--- Modules/HousingMovements/Resources/views/index.blade.php
+++ Modules/HousingMovements/Resources/views/index.blade.php
@@ -1,5 +1,8 @@
-@extends('housingmovements::layouts.master')
-
+@extends('layouts.app')
 @section('content')
+<h1>Hello World</h1>
 
+<p>
+    This view is loaded from module: {!! config('housingmovements.name') !!}
+</p>
 @endsection
