b85e4e2ba9f939cc1110e8f22505b3dab72a70ff
Rama Abbas <r.abbas@sahmtech.com>
2023-11-16 14:08:55 +0300

    ..

---
+++ Modules/FollowUp/Database/Migrations/2023_11_16_162301_add_to_followup_worker_requests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('followup_worker_requests', function (Blueprint $table) {
+            $table->unsignedInteger('updated_by')->after('type')->nullable();
+            $table->foreign('updated_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('', function (Blueprint $table) {
+
+        });
+    }
+};
