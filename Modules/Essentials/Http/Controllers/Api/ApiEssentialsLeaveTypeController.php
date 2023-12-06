f0dd92765a497bd5c4ef6ef5395ea22e699224c8
vicSahmtech <m.ahmad@sahmtech.com>
2023-12-07 00:08:34 +0300

    .

---
+++ Modules/Essentials/Http/Controllers/Api/ApiEssentialsLeaveTypeController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Modules\Essentials\Http\Controllers\Api;
+
+use App\Utils\ModuleUtil;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+use Modules\Connector\Transformers\CommonResource;
+use Modules\Essentials\Entities\EssentialsLeaveType;
+
+class ApiEssentialsLeaveTypeController extends Controller
+{
+    /**
+     * All Utils instance.
+     */
+    protected $moduleUtil;
+
+    /**
+     * Constructor
+     *
+     * @param  ProductUtils  $product
+     * @return void
+     */
+    public function __construct(ModuleUtil $moduleUtil)
+    {
+        $this->moduleUtil = $moduleUtil;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function getLeaveTypes()
+    {
+        $business_id = request()->session()->get('user.business_id');
+
+        if (!$this->moduleUtil->isModuleInstalled('Essentials')) {
+            abort(403, 'Unauthorized action.');
+        }
+
+        try {
+            $user = Auth::user();
+            $business_id = $user->business_id;
+
+
+            $leave_types = EssentialsLeaveType::where('business_id', $business_id)
+                ->select(['leave_type', 'duration', 'max_leave_count', 'id']);
+
+
+
+            $res = [
+                'leave_types' => $leave_types
+            ];
+
+            return new CommonResource($res);
+        } catch (\Exception $e) {
+            \Log::emergency('File:' . $e->getFile() . 'Line:' . $e->getLine() . 'Message:' . $e->getMessage());
+
+            return $this->otherExceptions($e);
+        }
+    }
+}
