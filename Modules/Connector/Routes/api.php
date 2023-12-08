258d1035d2d9b2a921c73330972a3b1d4badfee4
vicSahmtech <m.ahmad@sahmtech.com>
2023-12-08 23:05:42 +0300

    ..

--- Modules/Connector/Routes/api.php
+++ Modules/Connector/Routes/api.php
@@ -100,4 +100,5 @@ Route::middleware('auth:api', 'timezone')->prefix('connector/api/essentials')->g
     Route::get('getLeaveTypes', [Modules\Essentials\Http\Controllers\Api\ApiEssentialsLeaveTypeController::class, 'getLeaveTypes']);
     Route::get('getMyRequests', [Modules\FollowUp\Http\Controllers\Api\ApiFollowUpRequestController::class, 'getMyRequests']);
     Route::get('getMyToDo', [Modules\Essentials\Http\Controllers\Api\ApiEssentialsLeaveTypeController::class, 'getMyToDo']);
+    Route::post('makeRequest', [Modules\Essentials\Http\Controllers\Api\ApiEssentialsLeaveTypeController::class, 'makeRequest']);
 });
