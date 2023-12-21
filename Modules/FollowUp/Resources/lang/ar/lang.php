78649474f60da13aa3425d5fb9b4bf6e0be13d0f
MHD_Amen <mohammadamen042@gmail.com>
2023-12-21 03:27:57 +0300

    feat(permissions and translation):Add permissions to manage follow-up and add the translation file

--- Modules/FollowUp/Resources/lang/ar/lang.php
+++ Modules/FollowUp/Resources/lang/ar/lang.php
@@ -253,7 +253,8 @@ return [
     'doc_name_ar' => 'اسم السند بالعربية',
     'doc_name_en' => 'اسم السند بالانكليزية',
     'add_document' => 'إضافة سند',
-
+    'shifts' => 'فترات الدوام',
+    'contact_locations' => 'فروع المشاريع',
     'document_delivery' => 'سندات التسليم',
     'nots' => 'الملاحظات',
     'no_document_delivery_to_show' => 'لا يوجد سندات',
