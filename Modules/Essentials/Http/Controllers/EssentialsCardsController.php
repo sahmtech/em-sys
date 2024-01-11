a976aae03a27f29449789a654b40ba270a3b0685
Victor-Ahmad <m.ahmad@sahmtech.com>
2024-01-11 10:30:08 +0300

    ..

--- Modules/Essentials/Http/Controllers/EssentialsCardsController.php
+++ Modules/Essentials/Http/Controllers/EssentialsCardsController.php
@@ -626,7 +626,7 @@ class EssentialsCardsController extends Controller
         $users = User::with(['userAllowancesAndDeductions'])->where(function ($query) use ($user_businesses_ids, $user_projects_ids) {
             $query->where(function ($query2) use ($user_businesses_ids) {
                 $query2->whereIn('users.business_id', $user_businesses_ids)->whereIn('user_type', ['employee', 'manager', 'worker']);
-            })->orWhere(function ($query3) use ($user_projects_ids) {
+            })->orWhere(function ($query3) use ($user_projects_ids,$user_businesses_ids) {
                 $query3->where('user_type', 'worker')->whereIn('assigned_to', $user_projects_ids)->whereIn('users.business_id', $user_businesses_ids);
             });
         })->where('users.is_cmmsn_agnt', 0)
