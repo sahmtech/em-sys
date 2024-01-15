f2039acd0d6f2f9eb9f721eb6333b9616cb6edb7
Victor-Ahmad <m.ahmad@sahmtech.com>
2024-01-15 11:10:29 +0300

    update role edit.blade.php

--- resources/views/role/edit.blade.php
+++ resources/views/role/edit.blade.php
@@ -27,7 +27,11 @@
                 <div class="col-md-4">
                     <div class="form-group">
                         {!! Form::label('name', __('user.role_name') . ':*') !!}
-                        {!! Form::text('name', null, ['class' => 'form-control', 'required', 'placeholder' => __('user.role_name')]) !!}
+                        {!! Form::text('name', str_replace('#' . auth()->user()->business_id, '', $role->name), [
+                            'class' => 'form-control',
+                            'required',
+                            'placeholder' => __('user.role_name'),
+                        ]) !!}
                     </div>
                 </div>
             </div>
