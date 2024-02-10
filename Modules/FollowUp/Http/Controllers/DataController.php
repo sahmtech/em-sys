f2c2ea0e6d9983a0cdb050b74800f8000e3f6340
Rama Abbas <r.abbas@sahmtech.com>
2024-02-11 00:44:32 +0300

    .

--- Modules/FollowUp/Http/Controllers/DataController.php
+++ Modules/FollowUp/Http/Controllers/DataController.php
@@ -19,160 +19,160 @@ class DataController extends Controller
      *
      * @return array
      */
-    public function parse_notification($notification)
-    {
-        $notification_data = [];
-        if (
-            $notification->type ==
-            'Modules\followup\Notifications\DocumentShareNotification'
-        ) {
-            $notifiction_data = DocumentShare::documentShareNotificationData($notification->data);
-            $notification_data = [
-                'msg' => $notifiction_data['msg'],
-                'icon_class' => $notifiction_data['icon'],
-                'link' => $notifiction_data['link'],
-                'read_at' => $notification->read_at,
-                'created_at' => $notification->created_at->diffForHumans(),
-            ];
-        } elseif (
-            $notification->type ==
-            'Modules\followup\Notifications\NewMessageNotification'
-        ) {
-            $data = $notification->data;
-            $msg = __('followup::lang.new_message_notification', ['sender' => $data['from']]);
+    // public function parse_notification($notification)
+    // {
+    //     $notification_data = [];
+    //     if (
+    //         $notification->type ==
+    //         'Modules\followup\Notifications\DocumentShareNotification'
+    //     ) {
+    //         $notifiction_data = DocumentShare::documentShareNotificationData($notification->data);
+    //         $notification_data = [
+    //             'msg' => $notifiction_data['msg'],
+    //             'icon_class' => $notifiction_data['icon'],
+    //             'link' => $notifiction_data['link'],
+    //             'read_at' => $notification->read_at,
+    //             'created_at' => $notification->created_at->diffForHumans(),
+    //         ];
+    //     } elseif (
+    //         $notification->type ==
+    //         'Modules\followup\Notifications\NewMessageNotification'
+    //     ) {
+    //         $data = $notification->data;
+    //         $msg = __('followup::lang.new_message_notification', ['sender' => $data['from']]);
 
-            $notification_data = [
-                'msg' => $msg,
-                'icon_class' => 'fas fa-envelope bg-green',
-                'link' => action([\Modules\followup\Http\Controllers\followupMessageController::class, 'index']),
-                'read_at' => $notification->read_at,
-                'created_at' => $notification->created_at->diffForHumans(),
-            ];
-        } elseif (
-            $notification->type ==
-            'Modules\followup\Notifications\NewLeaveNotification'
-        ) {
-            $data = $notification->data;
+    //         $notification_data = [
+    //             'msg' => $msg,
+    //             'icon_class' => 'fas fa-envelope bg-green',
+    //             'link' => action([\Modules\followup\Http\Controllers\followupMessageController::class, 'index']),
+    //             'read_at' => $notification->read_at,
+    //             'created_at' => $notification->created_at->diffForHumans(),
+    //         ];
+    //     } elseif (
+    //         $notification->type ==
+    //         'Modules\followup\Notifications\NewLeaveNotification'
+    //     ) {
+    //         $data = $notification->data;
 
-            $employee = User::find($data['applied_by']);
+    //         $employee = User::find($data['applied_by']);
 
-            if (!empty($employee)) {
-                $msg = __('followup::lang.new_leave_notification', ['employee' => $employee->user_full_name, 'ref_no' => $data['ref_no']]);
+    //         if (!empty($employee)) {
+    //             $msg = __('followup::lang.new_leave_notification', ['employee' => $employee->user_full_name, 'ref_no' => $data['ref_no']]);
 
-                $notification_data = [
-                    'msg' => $msg,
-                    'icon_class' => 'fas fa-user-times bg-green',
-                    'link' => action([\Modules\followup\Http\Controllers\followupLeaveController::class, 'index']),
-                    'read_at' => $notification->read_at,
-                    'created_at' => $notification->created_at->diffForHumans(),
-                ];
-            }
-        } elseif (
-            $notification->type ==
-            'Modules\followup\Notifications\LeaveStatusNotification'
-        ) {
-            $data = $notification->data;
+    //             $notification_data = [
+    //                 'msg' => $msg,
+    //                 'icon_class' => 'fas fa-user-times bg-green',
+    //                 'link' => action([\Modules\followup\Http\Controllers\followupLeaveController::class, 'index']),
+    //                 'read_at' => $notification->read_at,
+    //                 'created_at' => $notification->created_at->diffForHumans(),
+    //             ];
+    //         }
+    //     } elseif (
+    //         $notification->type ==
+    //         'Modules\followup\Notifications\LeaveStatusNotification'
+    //     ) {
+    //         $data = $notification->data;
 
-            $admin = User::find($data['changed_by']);
+    //         $admin = User::find($data['changed_by']);
 
-            if (!empty($admin)) {
-                $msg = __('followup::lang.status_change_notification', ['status' => $data['status'], 'ref_no' => $data['ref_no'], 'admin' => $admin->user_full_name]);
+    //         if (!empty($admin)) {
+    //             $msg = __('followup::lang.status_change_notification', ['status' => $data['status'], 'ref_no' => $data['ref_no'], 'admin' => $admin->user_full_name]);
 
-                $notification_data = [
-                    'msg' => $msg,
-                    'icon_class' => 'fas fa-user-times bg-green',
-                    'link' => action([\Modules\followup\Http\Controllers\followupLeaveController::class, 'index']),
-                    'read_at' => $notification->read_at,
-                    'created_at' => $notification->created_at->diffForHumans(),
-                ];
-            }
-        } elseif (
-            $notification->type ==
-            'Modules\followup\Notifications\PayrollNotification'
-        ) {
-            $data = $notification->data;
+    //             $notification_data = [
+    //                 'msg' => $msg,
+    //                 'icon_class' => 'fas fa-user-times bg-green',
+    //                 'link' => action([\Modules\followup\Http\Controllers\followupLeaveController::class, 'index']),
+    //                 'read_at' => $notification->read_at,
+    //                 'created_at' => $notification->created_at->diffForHumans(),
+    //             ];
+    //         }
+    //     } elseif (
+    //         $notification->type ==
+    //         'Modules\followup\Notifications\PayrollNotification'
+    //     ) {
+    //         $data = $notification->data;
 
-            $month = \Carbon::createFromFormat('m', $data['month'])->format('F');
+    //         $month = \Carbon::createFromFormat('m', $data['month'])->format('F');
 
-            $msg = '';
+    //         $msg = '';
 
-            $created_by = User::find($data['created_by']);
+    //         $created_by = User::find($data['created_by']);
 
-            if (!empty($created_by)) {
-                if ($data['action'] == 'created') {
-                    $msg = __('followup::lang.payroll_added_notification', ['month_year' => $month . '/' . $data['year'], 'ref_no' => $data['ref_no'], 'created_by' => $created_by->user_full_name]);
-                } elseif ($data['action'] == 'updated') {
-                    $msg = __('followup::lang.payroll_updated_notification', ['month_year' => $month . '/' . $data['year'], 'ref_no' => $data['ref_no'], 'created_by' => $created_by->user_full_name]);
-                }
+    //         if (!empty($created_by)) {
+    //             if ($data['action'] == 'created') {
+    //                 $msg = __('followup::lang.payroll_added_notification', ['month_year' => $month . '/' . $data['year'], 'ref_no' => $data['ref_no'], 'created_by' => $created_by->user_full_name]);
+    //             } elseif ($data['action'] == 'updated') {
+    //                 $msg = __('followup::lang.payroll_updated_notification', ['month_year' => $month . '/' . $data['year'], 'ref_no' => $data['ref_no'], 'created_by' => $created_by->user_full_name]);
+    //             }
 
-                $notification_data = [
-                    'msg' => $msg,
-                    'icon_class' => 'fas fa-money-bill-alt bg-green',
-                    'link' => action([\Modules\followup\Http\Controllers\PayrollController::class, 'index']),
-                    'read_at' => $notification->read_at,
-                    'created_at' => $notification->created_at->diffForHumans(),
-                ];
-            }
-        } elseif (
-            $notification->type ==
-            'Modules\followup\Notifications\NewTaskNotification'
-        ) {
-            $data = $notification->data;
+    //             $notification_data = [
+    //                 'msg' => $msg,
+    //                 'icon_class' => 'fas fa-money-bill-alt bg-green',
+    //                 'link' => action([\Modules\followup\Http\Controllers\PayrollController::class, 'index']),
+    //                 'read_at' => $notification->read_at,
+    //                 'created_at' => $notification->created_at->diffForHumans(),
+    //             ];
+    //         }
+    //     } elseif (
+    //         $notification->type ==
+    //         'Modules\followup\Notifications\NewTaskNotification'
+    //     ) {
+    //         $data = $notification->data;
 
-            $assigned_by = User::find($data['assigned_by']);
+    //         $assigned_by = User::find($data['assigned_by']);
 
-            if (!empty($assigned_by)) {
-                $msg = __('followup::lang.new_task_notification', ['assigned_by' => $assigned_by->user_full_name, 'task_id' => $data['task_id']]);
+    //         if (!empty($assigned_by)) {
+    //             $msg = __('followup::lang.new_task_notification', ['assigned_by' => $assigned_by->user_full_name, 'task_id' => $data['task_id']]);
 
-                $notification_data = [
-                    'msg' => $msg,
-                    'icon_class' => 'ion ion-clipboard bg-green',
-                    'link' => action([\Modules\followup\Http\Controllers\ToDoController::class, 'show'], $data['id']),
-                    'read_at' => $notification->read_at,
-                    'created_at' => $notification->created_at->diffForHumans(),
-                ];
-            }
-        } elseif (
-            $notification->type ==
-            'Modules\followup\Notifications\NewTaskCommentNotification'
-        ) {
-            $data = $notification->data;
+    //             $notification_data = [
+    //                 'msg' => $msg,
+    //                 'icon_class' => 'ion ion-clipboard bg-green',
+    //                 'link' => action([\Modules\followup\Http\Controllers\ToDoController::class, 'show'], $data['id']),
+    //                 'read_at' => $notification->read_at,
+    //                 'created_at' => $notification->created_at->diffForHumans(),
+    //             ];
+    //         }
+    //     } elseif (
+    //         $notification->type ==
+    //         'Modules\followup\Notifications\NewTaskCommentNotification'
+    //     ) {
+    //         $data = $notification->data;
 
-            $comment = followupTodoComment::with(['task', 'added_by'])->find($data['comment_id']);
-            if (!empty($comment) && $comment->task) {
-                $msg = __('followup::lang.new_task_comment_notification', ['added_by' => $comment->added_by->user_full_name, 'task_id' => $comment->task->task_id]);
+    //         $comment = followupTodoComment::with(['task', 'added_by'])->find($data['comment_id']);
+    //         if (!empty($comment) && $comment->task) {
+    //             $msg = __('followup::lang.new_task_comment_notification', ['added_by' => $comment->added_by->user_full_name, 'task_id' => $comment->task->task_id]);
 
-                $notification_data = [
-                    'msg' => $msg,
-                    'icon_class' => 'fas fa-envelope bg-green',
-                    'link' => action([\Modules\followup\Http\Controllers\ToDoController::class, 'show'], $comment->task->id),
-                    'read_at' => $notification->read_at,
-                    'created_at' => $notification->created_at->diffForHumans(),
-                ];
-            }
-        } elseif (
-            $notification->type ==
-            'Modules\followup\Notifications\NewTaskDocumentNotification'
-        ) {
-            $data = $notification->data;
+    //             $notification_data = [
+    //                 'msg' => $msg,
+    //                 'icon_class' => 'fas fa-envelope bg-green',
+    //                 'link' => action([\Modules\followup\Http\Controllers\ToDoController::class, 'show'], $comment->task->id),
+    //                 'read_at' => $notification->read_at,
+    //                 'created_at' => $notification->created_at->diffForHumans(),
+    //             ];
+    //         }
+    //     } elseif (
+    //         $notification->type ==
+    //         'Modules\followup\Notifications\NewTaskDocumentNotification'
+    //     ) {
+    //         $data = $notification->data;
 
-            $uploaded_by = User::find($data['uploaded_by']);
+    //         $uploaded_by = User::find($data['uploaded_by']);
 
-            if (!empty($uploaded_by)) {
-                $msg = __('followup::lang.new_task_document_notification', ['uploaded_by' => $uploaded_by->user_full_name, 'task_id' => $data['task_id']]);
+    //         if (!empty($uploaded_by)) {
+    //             $msg = __('followup::lang.new_task_document_notification', ['uploaded_by' => $uploaded_by->user_full_name, 'task_id' => $data['task_id']]);
 
-                $notification_data = [
-                    'msg' => $msg,
-                    'icon_class' => 'fas fa-file bg-green',
-                    'link' => action([\Modules\followup\Http\Controllers\ToDoController::class, 'show'], $data['id']),
-                    'read_at' => $notification->read_at,
-                    'created_at' => $notification->created_at->diffForHumans(),
-                ];
-            }
-        }
+    //             $notification_data = [
+    //                 'msg' => $msg,
+    //                 'icon_class' => 'fas fa-file bg-green',
+    //                 'link' => action([\Modules\followup\Http\Controllers\ToDoController::class, 'show'], $data['id']),
+    //                 'read_at' => $notification->read_at,
+    //                 'created_at' => $notification->created_at->diffForHumans(),
+    //             ];
+    //         }
+    //     }
 
-        return $notification_data;
-    }
+    //     return $notification_data;
+    // }
 
     /**
      * Defines user permissions for the module.
@@ -221,6 +221,13 @@ class DataController extends Controller
                         'default' => false,
 
                     ],
+                    [
+                        'value' => 'followup.cancle_worker_project',
+                        'label' => __('followup::lang.cancle_worker_project'),
+                        'default' => false,
+
+                    ],
+                    
                     [
                         'value' => 'followup.view_worker_details',
                         'label' => __('followup::lang.view_workerProject_details'),
