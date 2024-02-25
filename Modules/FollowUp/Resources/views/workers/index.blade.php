22d192d18e577d61a74f4eb35d451bf20231c033
rahafMouty <rahafmoute745@gmail.com>
2024-02-25 11:38:54 +0300

    --

--- Modules/FollowUp/Resources/views/workers/index.blade.php
+++ Modules/FollowUp/Resources/views/workers/index.blade.php
@@ -60,7 +60,10 @@
                         </div>
                     </div>
                     @php
-                        $default_fields = [$fields[0], $fields[1], $fields[2], $fields[3], $fields[4], $fields[5], $fields[6]];
+                        $default_fields = [$fields[0], $fields[1], $fields[2], $fields[3], $fields[4], $fields[5], $fields[6],
+                         $fields[7],$fields[8],$fields[9] ,$fields[10] ,$fields[11],
+                         $fields[12],$fields[13],$fields[14] ,$fields[15] ,$fields[16],
+                          $fields[17],$fields[18],$fields[19] ,$fields[20] ,$fields[21]];
 
                         $default = array_keys($default_fields);
 
