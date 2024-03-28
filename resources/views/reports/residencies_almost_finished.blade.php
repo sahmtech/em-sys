522b68a3de647651c17d3767b46105203fb01802
MHD_Amen <mohammadamen042@gmail.com>
2024-03-28 14:29:22 +0300

    feat(reports):add reports

---
+++ resources/views/reports/residencies_almost_finished.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.app')
+@section('title', __('essentials::lang.residencies_almost_finished'))
+
+@section('content')
+
+    <section class="content-header">
+
+        <h1>@lang('essentials::lang.residencies_almost_finished')
+        </h1>
+
+        <section class="content">
+
+            <div class="row">
+                <div class="col-md-12">
+                    @component('components.widget', ['class' => 'box-solid'])
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-striped" id="residencies_almost_finished">
+                                <thead>
+                                    <tr>
+
+                                        <th>@lang('followup::lang.worker_name')</th>
+                                        <th>@lang('followup::lang.residency')</th>
+                                        <th>@lang('followup::lang.project')</th>
+                                        <th>@lang('followup::lang.customer_name')</th>
+                                        <th>@lang('followup::lang.end_date')</th>
+                                        <th></th>
+
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    @endcomponent
+
+                </div>
+            </div>
+
+        </section>
+    @endsection
+
+    @section('javascript')
+        <script type="text/javascript">
+            $(document).ready(function() {
+
+
+                var residencies_almost_finished;
+
+                function reloadDataTable() {
+                    residencies_almost_finished.ajax.reload();
+                }
+
+                residencies_almost_finished = $('#residencies_almost_finished').DataTable({
+                    processing: true,
+                    serverSide: true,
+                    ajax: {
+                        url: "{{ route('residencies_almost_finished') }}",
+
+                    },
+
+                    columns: [
+
+                        {
+                            data: 'worker_name'
+                        },
+                        {
+                            data: 'residency'
+                        },
+                        {
+                            data: 'project'
+                        },
+                        {
+                            data: 'customer_name'
+                        },
+                        {
+                            data: 'end_date'
+                        },
+                        {
+                            data: 'action'
+                        },
+                    ],
+                });
+
+
+
+
+            });
+        </script>
+    @endsection
