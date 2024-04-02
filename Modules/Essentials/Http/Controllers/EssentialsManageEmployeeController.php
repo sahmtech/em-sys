0fc6738b0659cc448c44974655308120e66c36d3
Victor-Ahmad <m.ahmad@sahmtech.com>
2024-04-02 12:22:40 +0300

    update EssentialsManageEmployeeController.php

--- Modules/Essentials/Http/Controllers/EssentialsManageEmployeeController.php
+++ Modules/Essentials/Http/Controllers/EssentialsManageEmployeeController.php
@@ -1593,7 +1593,6 @@ class EssentialsManageEmployeeController extends Controller
         } catch (\Exception $e) {
             DB::rollBack();
             error_log($e->getMessage());
-            return 'File:' . $e->getFile() . 'Line:' . $e->getLine() . 'Message:' . $e->getMessage();
             \Log::emergency('File:' . $e->getFile() . 'Line:' . $e->getLine() . 'Message:' . $e->getMessage());
             $output = [
                 'success' => 0,
