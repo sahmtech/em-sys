99890bc9a790c3bab7abb1e381a8a2f4026a0a02
MHD_Amen <mohammadamen042@gmail.com>
2024-04-23 11:06:11 +0300

    ..

--- resources/views/reports/expired_residencies.blade.php
+++ resources/views/reports/expired_residencies.blade.php
@@ -4,14 +4,15 @@
 @section('content')
 
     <section class="content-header">
-<head>
-    <style>
-    .bg-green {
-        background-color: #28a745; 
-        color: #ffffff; 
-    }
-</style>
-</head>
+
+        <head>
+            <style>
+                .bg-green {
+                    background-color: #28a745;
+                    color: #ffffff;
+                }
+            </style>
+        </head>
         <h1>@lang('essentials::lang.allexpired_residencies')
         </h1>
 
@@ -66,15 +67,17 @@
                     processing: true,
                     serverSide: true,
                     footer: true,
-                                    buttons: ['excel', {
-                                                extend: 'print',
-                                                title: ' ',
-                                                text: '<i class="glyphicon glyphicon-print" style="padding: 0px 7px"></i><span>Print</span>',
-                                                className: 'btn printclass textSan',
-                                                customize: function(win) {
-                                                        $(win.document.body).prepend('<div style="display: flex;justify-content: space-between;"><div class="row" style="padding: 5px 25px;"><h3>@lang('lang_v1.emdadatalatta_comp')</h3><h3>@lang('essentials::lang.work_cards')</h3><h4>@lang('lang_v1.report') @lang('essentials::lang.allexpired_residencies')</h4></div><img src="/uploads/custom_logo.png" class="img-rounded" alt="Logo" style="width: 175px;"> </div>');
-        }
-        }],
+                    buttons: ['excel', {
+                        extend: 'print',
+                        title: ' ',
+                        text: '<i class="glyphicon glyphicon-print" style="padding: 0px 7px"></i><span>Print</span>',
+                        className: 'btn printclass textSan',
+                        customize: function(win) {
+                            $(win.document.body).prepend(
+                                '<div style="display: flex;justify-content: space-between;"><div class="row" style="padding: 5px 25px;"><h3>@lang('lang_v1.emdadatalatta_comp')</h3><h3>@lang('essentials::lang.work_cards')</h3><h4>@lang('lang_v1.report') @lang('essentials::lang.allexpired_residencies')</h4></div><img src="/uploads/custom_logo.png" class="img-rounded" alt="Logo" style="width: 175px;"> </div>'
+                                );
+                        }
+                    }],
                     ajax: {
                         url: "{{ route('expired_residencies') }}",
 
@@ -82,59 +85,45 @@
 
                     columns: [
 
-                        {
-                            data: 'worker_name', searchable: true 
-                        },
-                        {
-                            data: 'residency', searchable: true 
-                        },
-                        
-                        {
-                            data: 'end_date', searchable: true 
-                        },
-                        {
-                            "data": "company_name", searchable: true 
-                        },
-                        {
-                            data: 'passport_number', searchable: true 
-                        },
-                        {
-                            data: 'passport_expire_date', searchable: true 
-                        },
-                        {
-                            data: 'border_no', searchable: true 
-                        },
-                        {
-                            data: 'dob', searchable: true 
-                        },
-                         {
-                            data: 'gender',, searchable: true ,
-                             render: function(data, type, row) {
-                            if (data === 'male') {
-                                return '@lang('lang_v1.male')';
-                            } else if (data === 'female') {
-                                return '@lang('lang_v1.female')';
+{
+    data: 'worker_name'
+},
+{
+    data: 'residency'
+},
+{
+    "data": "company_name"
+},
+{
+    data: 'passport_number'
+},
+{
+    data: 'passport_expire_date'
+},
+{
+    data: 'border_no'
+}, {
+    data: 'dob'
+},
+{
+    data: 'project'
+},
+{
+    data: 'customer_name'
+},
 
-                            } else {
-                                return '@lang('lang_v1.others')';
-                            }
-                        }
-                        },
-                        {
-                            data: 'customer_name', searchable: true 
-                        },
-                        {
-                            data: 'project', searchable: true 
-                        },
-                        
-
-                        {
-                            data: 'nationality', searchable: true 
-                        }, {
-                            data: "profession", searchable: true 
-                            name: 'profession', searchable: true 
-                        },
-                    ],
+{
+    data: 'end_date'
+},
+{
+    data: 'nationality'
+}, {
+    data: "profession",
+    name: 'profession'
+}, {
+    data: "gender",
+},
+],
                 });
 
 
