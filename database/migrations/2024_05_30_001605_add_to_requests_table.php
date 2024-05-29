406cb1b37a617779aef3d697e7dbbb32478cef54
Rama Abbas <r.abbas@sahmtech.com>
2024-05-29 22:42:13 +0300

    .

---
+++ database/migrations/2024_05_30_001605_add_to_requests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('requests', function (Blueprint $table) {
+            $table->unsignedInteger('updated_by')->nullable()->after('created_by');
+            $table->boolean('is_new')->after('status')->default('1');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('requests', function (Blueprint $table) {
+            //
+        });
+    }
+};
