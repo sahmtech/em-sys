bbaa371bddbcf0748d101430c268770910110b99
Victor-Ahmad <victor.ahmad.mail@gmail.com>
2024-06-13 12:13:54 +0300

    ..

--- Modules/Sales/Http/Controllers/ClientsController.php
+++ Modules/Sales/Http/Controllers/ClientsController.php
@@ -387,7 +387,7 @@ class ClientsController extends Controller
 
                 //add contact as user can't log in
                 $userInfo['user_type'] = 'customer';
-                $userInfo['first_name'] = $request->supplier_business_name;
+                $userInfo['first_name'] = $contact_input['supplier_business_name'];
                 $userInfo['allow_login'] = 0;
                 $userInfo['business_id'] =  $business_id;
                 $userInfo['crm_contact_id'] =  $contactId;
