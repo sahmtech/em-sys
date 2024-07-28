c76e317686a033da18442f65f847fe990707eda4
Victor-Ahmad <victor.ahmad.mail@gmail.com>
2024-07-28 13:40:39 +0300

    .

--- Modules/Essentials/Http/Controllers/PayrollController.php
+++ Modules/Essentials/Http/Controllers/PayrollController.php
@@ -1029,7 +1029,7 @@ class PayrollController extends Controller
         $employee_ids = User::with('contract');
 
         if ($user_type == "worker") {
-            $employee_ids = $employee_ids->whereIn('assigned_to', $projects_ids)->where('user_type', 'worker');
+            $employee_ids = $employee_ids->whereIn('company_id', $companies_ids)->whereIn('assigned_to', $projects_ids)->where('user_type', 'worker');
         } elseif ($user_type == "employee" || $user_type == "remote_employee") {
             $employee_ids = $employee_ids->whereIn('company_id', $companies_ids)->where('user_type', 'employee');
         }
--- Modules/Essentials/Resources/views/payroll/index.blade.php
+++ Modules/Essentials/Resources/views/payroll/index.blade.php
@@ -14,7 +14,7 @@
 
                 <div class="col-md-12">
                     @if (auth()->user()->hasRole('Admin#1') || auth()->user()->can('essentials.create_payroll'))
-                        <button type="button" class="btn btn-primary " data-toggle="modal" data-target="#addPayrollModal">
+                        <button type="button" class="btn btn-primary " data-toggle="modal" data-target="#payroll_modal">
                             <i class="fa fa-plus"></i>
                             @lang('messages.add')
                         </button>
@@ -63,79 +63,158 @@
     </section>
     <!-- /.content -->
 
-    <!-- Add Payroll Modal -->
-    <div class="modal fade" id="addPayrollModal" tabindex="-1" role="dialog" aria-labelledby="addPayrollModalLabel">
+    <div class="modal fade" id="payroll_modal" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title" id="addPayrollModalLabel">@lang('essentials::lang.add_payroll')</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                            aria-hidden="true">&times;</span></button>
-                </div>
-                <div class="modal-body">
-                    <form id="addPayrollForm" action="{{ route('payrolls.create') }}" method="GET">
-                        <div class="form-group">
-                            <label for="user_type">@lang('essentials::lang.user_type')</label>
-                            <select class="form-control" id="user_type" name="user_type">
-                                <option value="worker">@lang('essentials::lang.worker')</option>
-                                <option value="employee">@lang('essentials::lang.employee')</option>
-                            </select>
-                        </div>
-                        <div class="form-group" id="projects_container">
-                            <label for="projects">@lang('essentials::lang.project')</label>
-                            <select class="form-control select2" id="projects" name="projects[]" multiple>
-                                @foreach ($projects as $id => $name)
-                                    <option value="{{ $id }}">{{ $name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="form-group" id="companies_container">
-                            <label for="companies">@lang('essentials::lang.company')</label>
-                            <select class="form-control select2" id="companies" name="companies[]" multiple>
-                                @foreach ($companies as $id => $name)
-                                    <option value="{{ $id }}">{{ $name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        {{-- <div class="form-group" id="projects_container" style="display: none;">
-                            {!! Form::label('project_name', __('request.project_name') . ':*') !!}
-                            {!! Form::select('project_name', $projects, null, [
-                                'class' => 'form-control select2',
-                                'style' => 'height: 40px',
-                                'multiple' => 'multiple',
-                                'name' => 'projects[]',
-                                'placeholder' => __('request.select_project'),
-                            ]) !!}
-                        </div>
-                        <div class="form-group" id="companies_container" style="display: none;">
-                            {!! Form::label('companies', __('essentials::lang.companies') . ':*') !!}
-                            {!! Form::select('companies', $companies, null, [
-                                'class' => 'form-control select2',
-                                'style' => 'height: 40px',
-                                'multiple' => 'multiple',
-                                'name' => 'companies[]',
-                                'placeholder' => __('essentials::lang.companies'),
-                            ]) !!}
-                        </div> --}}
 
-                        <div class="form-group">
-                            {!! Form::label('month_year', __('essentials::lang.month_year') . ':*') !!}
-                            <div class="input-group">
-                                {!! Form::text('month_year', null, [
-                                    'class' => 'form-control',
-                                    'placeholder' => __('essentials::lang.month_year'),
-                                    'required',
-                                    'readonly',
+                {!! Form::open([
+                    'url' => route('payrolls.create'),
+                    'method' => 'get',
+                    'id' => 'add_payroll_step1',
+                ]) !!}
+                <div class="modal-header">
+                    <div class="col-md-12">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                {!! Form::label('companies', __('essentials::lang.company') . ':*') !!}
+                                {!! Form::select('companies[]', $companies, null, [
+                                    'class' => 'form-control select2',
+                                    'style' => 'width: 100%;',
+                                    'id' => 'companies',
+                                
+                                    'multiple',
+                                    // 'placeholder' => __('lang_v1.all'),
                                 ]) !!}
-                                <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
                             </div>
                         </div>
-                        <button type="submit" class="btn btn-primary">@lang('essentials::lang.add_payroll')</button>
-                    </form>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                {!! Form::label('user_type', __('essentials::lang.user_type') . ':*') !!}
+                                {!! Form::select('user_type', $user_types, null, [
+                                    'class' => 'form-control select2',
+                                    'style' => 'width: 100%;',
+                                    'id' => 'user_type',
+                                    'required',
+                                    // 'placeholder' => __('lang_v1.all'),
+                                ]) !!}
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                {!! Form::label('month_year', __('essentials::lang.month_year') . ':*') !!}
+                                <div class="input-group">
+                                    {!! Form::text('month_year', null, [
+                                        'class' => 'form-control',
+                                        'placeholder' => __('essentials::lang.month_year'),
+                                        'required',
+                                        'readonly',
+                                    ]) !!}
+                                    <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12 ">
+                            <div class="form-group" id="projects_container">
+                                {!! Form::label('projects', __('essentials::lang.sales_projects') . ':*') !!}
+                                {!! Form::select('projects[]', $projects, null, [
+                                    'class' => 'form-control select2',
+                                    'style' => 'width: 100%;',
+                                    'id' => 'projects',
+                                
+                                    'multiple',
+                                    // 'placeholder' => __('lang_v1.all'),
+                                ]) !!}
+                            </div>
+                        </div>
+
+
+
+                    </div>
+
+                    {{-- <div class="form-group">
+                        {!! Form::label('employee_ids', __('essentials::lang.employee') . ':*') !!}
+                        <button type="button" class="btn btn-primary btn-xs select-all">
+                            @lang('lang_v1.select_all')
+                        </button>
+                        <button type="button" class="btn btn-primary btn-xs deselect-all">
+                            @lang('lang_v1.deselect_all')
+                        </button>
+                        {!! Form::select('employee_ids[]', $employees, null, [
+                            'class' => 'form-control select2',
+                            'required',
+                            'style' => 'width: 100%;',
+                            'multiple',
+                            'id' => 'employee_ids',
+                        ]) !!}
+                    </div> --}}
+
+
+                </div>
+
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">@lang('essentials::lang.add_payroll')</button>
+                </div>
+
+                {!! Form::close() !!}
+
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div>
+
+
+    <!-- Add Payroll Modal -->
+    {{-- <div class="modal fade" id="addPayrollModal" tabindex="-1" role="dialog" aria-labelledby="addPayrollModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title" id="addPayrollModalLabel">@lang('essentials::lang.add_payroll')</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                                aria-hidden="true">&times;</span></button>
+                    </div>
+                    <div class="modal-body">
+                        <form id="addPayrollForm" action="{{ route('payrolls.create') }}" method="GET">
+                            <div class="form-group">
+                                <label for="user_type">@lang('essentials::lang.user_type')</label>
+                                <select class="form-control" id="user_type" name="user_type">
+                                    <option value="worker">@lang('essentials::lang.worker')</option>
+                                    <option value="employee">@lang('essentials::lang.employee')</option>
+                                </select>
+                            </div>
+                            <div class="form-group" id="projects_container">
+                                <label for="projects">@lang('essentials::lang.project')</label>
+                                <select class="form-control select2" id="projects" name="projects[]" multiple>
+                                    @foreach ($projects as $id => $name)
+                                        <option value="{{ $id }}">{{ $name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group" id="companies_container">
+                                <label for="companies">@lang('essentials::lang.company')</label>
+                                <select class="form-control select2" id="companies" name="companies[]" multiple>
+                                    @foreach ($companies as $id => $name)
+                                        <option value="{{ $id }}">{{ $name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                {!! Form::label('month_year', __('essentials::lang.month_year') . ':*') !!}
+                                <div class="input-group">
+                                    {!! Form::text('month_year', null, [
+                                        'class' => 'form-control',
+                                        'placeholder' => __('essentials::lang.month_year'),
+                                        'required',
+                                        'readonly',
+                                    ]) !!}
+                                    <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">@lang('essentials::lang.add_payroll')</button>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
+        </div> --}}
 
 @endsection
 @section('javascript')
@@ -143,7 +222,6 @@
         $(document).ready(function() {
             // Initially hide both containers
             $('#projects_container').hide();
-            $('#companies_container').hide();
 
             // Function to toggle visibility of project and company inputs
             function toggleProjectsAndCompanies() {
@@ -151,10 +229,8 @@
 
                 if (selectedUserType === 'worker') {
                     $('#projects_container').show();
-                    $('#companies_container').hide();
                 } else if (selectedUserType === 'employee') {
                     $('#projects_container').hide();
-                    $('#companies_container').show();
                 }
             }
 
