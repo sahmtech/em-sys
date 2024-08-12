332266f541045b44dbc343e03f65353c6b189cf4
mhd_amen042 <miladalmani03@gmail.com>
2024-08-12 08:44:56 +0300

    feat(Accounting):Adding the feature of selecting cost centers on automatic transfer restrictions Adding an automatic transfer that meets the condition of the path type (worker, employee, remote) Transfers salary restrictions according to the company through company_id

---
+++ Modules/Accounting/Database/Migrations/2024_08_12_025540_add_cloumaccounting_acc_trans_mapping_setting_auto_migrations.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('accounting_acc_trans_mapping_setting_auto_migrations', function (Blueprint $table) {
+            $table->text('cost_center_id')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('', function (Blueprint $table) {
+
+        });
+    }
+};
