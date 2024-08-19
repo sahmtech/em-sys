d40d5b7965b40d3385b5f412c308767931afec31
Rama Abbas <r.abbas@sahmtech.com>
2024-08-19 15:50:45 +0300

    ز

--- Modules/Accounting/Resources/views/requests/allRequest.blade.php
+++ Modules/Accounting/Resources/views/requests/allRequest.blade.php
@@ -2038,10 +2038,7 @@
                                     label: '{{ __('request.installmentsNumber') }}',
                                     value: requestInfo.installmentsNumber
                                 },
-                                {
-                                    label: '{{ __('request.baladyCardType') }}',
-                                    value: requestInfo.baladyCardType
-                                },
+
                                 {
                                     label: '{{ __('request.workInjuriesDate') }}',
                                     value: requestInfo.workInjuriesDate
