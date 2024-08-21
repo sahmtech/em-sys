4c5bf0e73efbab8adef997e5ede08040c1f8aa4b
Victor-Ahmad <victor.ahmad.mail@gmail.com>
2024-08-21 09:32:18 +0300

    test

--- Modules/Essentials/Http/Controllers/Api/ApiEssentialsController.php
+++ Modules/Essentials/Http/Controllers/Api/ApiEssentialsController.php
@@ -183,8 +183,8 @@ class ApiEssentialsController extends ApiController
                 // loan
                 $allowances_and_deductions = $this->getAllowanceAndDeductionJson($payrollUser);
 
-                $allowances = $allowances_and_deductions['essentials_allowances'];
-                $deductions = $allowances_and_deductions['essentials_deductions'];
+                $allowances = json_encode($allowances_and_deductions['essentials_allowances']);
+                $deductions = json_encode($allowances_and_deductions['essentials_deductions']);
 
                 $res[] = [
                     'payroll' => [
