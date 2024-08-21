ca4c8d763bee468d6665f54b57f15be6997e683c
Victor-Ahmad <victor.ahmad.mail@gmail.com>
2024-08-21 09:08:46 +0300

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends ConsoleKernel
                 ->emailOutputTo($email);
         }
 
-        $schedule->command('escalate:pending-requests')->dailyAt('09:05')->timezone('Asia/Riyadh');
+        $schedule->command('escalate:pending-requests')->dailyAt('09:10')->timezone('Asia/Riyadh');
         // $schedule->command('escalate:pending-requests')->everyMinute()->timezone('Asia/Riyadh');
         $schedule->command('populate:userleavebalances')->dailyAt('16:27')->timezone('Asia/Riyadh');
         $schedule->command('run:dialy-checks')->dailyAt('23:55')->timezone('Asia/Riyadh');
