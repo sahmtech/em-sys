bf481b887ea35d00c48ae442acb93cd53d704357
vic777or <victor.ahmad.mail@gmail.com>
2024-10-13 15:53:51 +0300

    added businessSector page

--- app/Http/Controllers/BusinessSectorController.php
+++ app/Http/Controllers/BusinessSectorController.php
@@ -4,14 +4,18 @@ namespace App\Http\Controllers;
 
 
 use App\Report;
-
+use App\User;
 use App\Utils\ModuleUtil;
-
-
+use Carbon\Carbon;
+use Modules\Essentials\Entities\EssentialsCountry;
+use Illuminate\Support\Facades\DB;
+use Modules\Essentials\Entities\EssentialsCity;
+use Yajra\DataTables\Facades\DataTables;
 
 class BusinessSectorController extends Controller
 {
     protected $moduleUtil;
+    protected $statuses;
     /**
      * Constructor
      *
@@ -20,11 +24,76 @@ class BusinessSectorController extends Controller
     public function __construct(ModuleUtil $moduleUtil)
     {
         $this->moduleUtil = $moduleUtil;
+        $this->statuses = [
+            'qualified' => [
+                'name' => __('sales::lang.qualified'),
+                'class' => 'bg-green',
+            ],
+            'unqualified' => [
+                'name' => __('sales::lang.unqualified'),
+                'class' => 'bg-red',
+            ],
+
+        ];
     }
 
     public function landing()
     {
 
-        return view('business_sector.index');
+
+
+        $business_id = request()->session()->get('user.business_id');
+        $is_admin = auth()->user()->hasRole('Admin#1') ? true : false;
+
+
+        $query = User::where('business_id', $business_id);
+        $all_users = $query->select('id', DB::raw("CONCAT(COALESCE(surname, ''),' ',COALESCE(first_name, ''),' ',COALESCE(last_name,'')) as full_name"))->get();
+        $users = $all_users->pluck('full_name', 'id');
+
+
+        $contacts = DB::table('contacts')
+            ->select([
+                'id',
+                'supplier_business_name',
+                'type',
+                'contact_id',
+                'created_by',
+                'created_at',
+                'commercial_register_no',
+                'mobile',
+                'email',
+                'city',
+                'note_draft',
+                'name'
+
+            ])->where('business_id', $business_id)->where('type', 'draft')->orderByDesc('created_at');
+        $cities = EssentialsCity::forDropdown();
+        if (request()->ajax()) {
+
+
+            return Datatables::of($contacts)
+
+
+
+                ->editColumn('created_by', function ($row) use ($users) {
+
+                    return $users[$row->created_by];
+                })
+                ->editColumn('created_at', function ($row) use ($users) {
+
+                    return Carbon::parse($row->created_at);
+                })
+
+                ->filterColumn('name', function ($query, $keyword) {
+                    $query->where('name', 'like', "%{$keyword}%");
+                })
+
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+
+        $status = $this->statuses;
+        $nationalities = EssentialsCountry::nationalityForDropdown();
+        return view('business_sector.index')->with(compact('users', 'status', 'cities', 'nationalities'));
     }
 }
--- resources/views/business_sector/index.blade.php
+++ resources/views/business_sector/index.blade.php
@@ -5,11 +5,42 @@
 
     <!-- Content Header (Page header) -->
     <section class="content-header">
-
+        <h1>
+            <span>@lang('sales::lang.draft_contacts')</span>
+        </h1>
     </section>
 
     <!-- Main content -->
     <section class="content">
+
+
+
+        @component('components.widget', ['class' => 'box-primary'])
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped" id="cust_table">
+                    <thead>
+                        <tr>
+                            {{-- <th>
+                                <input type="checkbox" id="select-all">
+                            </th> --}}
+                            <th>#</th>
+                            <th>@lang('sales::lang.contact_number')</th>
+                            <th>@lang('sales::lang.supplier_business_name')</th>
+                            <th>@lang('sales::lang.contact_name')</th>
+                            <th>@lang('sales::lang.created_by')</th>
+                            <th>@lang('sales::lang.contact_mobile')</th>
+                            <th>@lang('sales::lang.contact_email')</th>
+                            <th>@lang('sales::lang.message')</th>
+                            <th>@lang('sales::lang.created_at')</th>
+
+
+                        </tr>
+                    </thead>
+                </table>
+
+            </div>
+        @endcomponent
+
 
 
 
@@ -18,6 +49,77 @@
     <!-- /.content -->
 @stop
 
+
 @section('javascript')
+
+
+
+    <script>
+        $(document).ready(function() {
+
+
+            var customers_table = $('#cust_table').DataTable({
+                ajax: {
+                    url: "{{ route('businessSector') }}",
+
+                },
+                processing: true,
+                serverSide: true,
+                info: false,
+
+
+                columns: [
+
+                    {
+                        data: 'id',
+                        name: 'id'
+                    },
+                    {
+                        data: 'contact_id',
+                        name: 'contact_id'
+                    },
+                    {
+                        data: 'supplier_business_name',
+                        name: 'supplier_business_name'
+                    },
+                    {
+                        data: 'name',
+                        name: 'name'
+                    },
+
+
+
+                    {
+                        data: 'created_by',
+                        name: 'created_by'
+                    },
+                    {
+                        data: 'mobile',
+                        name: 'mobile'
+                    },
+                    {
+                        data: 'email',
+                        name: 'email'
+                    },
+                    {
+                        data: 'note_draft',
+                        name: 'note_draft'
+                    },
+                    {
+                        data: 'created_at',
+                        name: 'created_at'
+                    },
+
+
+                ]
+            });
+
+
+
+
+
+
+        });
+    </script>
 
 @endsection
