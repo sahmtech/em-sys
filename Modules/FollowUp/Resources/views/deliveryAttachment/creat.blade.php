a78322a2339babc0b6d80961462727fdfee8c231
waleedcode <waleedflutter@gmail.com>
2025-01-22 15:49:40 +0300

    Edit lable attachment_owner

--- Modules/FollowUp/Resources/views/deliveryAttachment/creat.blade.php
+++ Modules/FollowUp/Resources/views/deliveryAttachment/creat.blade.php
@@ -12,41 +12,45 @@
 
 
                     {!! Form::open([
-                        'url' => action('Modules\FollowUp\Http\Controllers\FollowupDeliveryAttachmentController@store'),
-                        'method' => 'post',
-                        'enctype' => 'multipart/form-data',
-                        'id' => 'doc_add_form',
+                    'url' => action('Modules\FollowUp\Http\Controllers\FollowupDeliveryAttachmentController@store'),
+                    'method' => 'post',
+                    'enctype' => 'multipart/form-data',
+                    'id' => 'doc_add_form',
                     ]) !!}
 
                     <div class="row">
 
                         <div class="col-md-6">
-                            {!! Form::label('worker', __('followup::lang.employee_or_worker')) !!}<span style="color: red; font-size:10px"> *</span>
+                            {!! Form::label('worker', __('followup::lang.attachment_owner')) !!}<span
+                                style="color: red; font-size:10px"> *</span>
                             <select class="form-control" required name="user_id" id="worker__select"
                                 style="padding: 2px;">
                                 <option value="">{{ __('followup::lang.select_employee_or_worker') }}</option>
                                 @foreach ($workers as $worker)
-                                    <option value="{{ $worker->id }}">
-                                        {{ $worker->id_proof_number . ' - ' . $worker->first_name . ' ' . $worker->mid_name . ' ' . $worker->last_name }}
-                                    </option>
+                                <option value="{{ $worker->id }}">
+                                    {{ $worker->id_proof_number . ' - ' . $worker->first_name . ' ' . $worker->mid_name
+                                    . ' ' . $worker->last_name }}
+                                </option>
                                 @endforeach
                             </select>
                         </div>
                         <div class="col-md-6">
-                            {!! Form::label('documents', __('followup::lang.attachment_type')) !!}<span style="color: red; font-size:10px"> *</span>
+                            {!! Form::label('documents', __('followup::lang.attachment_type')) !!}<span
+                                style="color: red; font-size:10px"> *</span>
                             <select class="form-control" required name="document_id" id="document_id"
                                 style="padding: 2px;">
                                 <option value="">{{ __('followup::lang.select_attach_type') }}</option>
                                 @foreach ($documents as $document)
-                                    <option value="{{ $document->id }}">
-                                        {{ $document->name_ar . ' - ' . $document->name_en }}
-                                    </option>
+                                <option value="{{ $document->id }}">
+                                    {{ $document->name_ar . ' - ' . $document->name_en }}
+                                </option>
                                 @endforeach
                             </select>
                         </div>
 
                         <div class="col-md-6" id="inputTitleContainer" style="display: none;">
-                            {!! Form::label('title', __('followup::lang.title')) !!}<span style="color: red; font-size:10px"> *</span>
+                            {!! Form::label('title', __('followup::lang.title')) !!}<span
+                                style="color: red; font-size:10px"> *</span>
                             {!! Form::text('title', null, ['class' => 'form-control', 'id' => 'title']) !!}
                         </div>
 
@@ -56,8 +60,10 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group">
-                                {!! Form::label('document', __('followup::lang.upload_attachment') . '  ') !!}<span style="color: red; font-size:10px"> *</span>
-                                {!! Form::file('document', ['class' => 'form-control', 'required', 'accept' => 'doc/*']) !!}
+                                {!! Form::label('document', __('followup::lang.upload_attachment') . ' ') !!}<span
+                                    style="color: red; font-size:10px"> *</span>
+                                {!! Form::file('document', ['class' => 'form-control', 'required', 'accept' => 'doc/*'])
+                                !!}
 
 
                             </div>
@@ -65,11 +71,11 @@
 
                         <div class="col-md-6">
                             <div class="form-group">
-                                {!! Form::label('nots', __('followup::lang.nots') . '  ') !!}
+                                {!! Form::label('nots', __('followup::lang.nots') . ' ') !!}
                                 {!! Form::text('nots', '', [
-                                    'class' => 'form-control',
-                                    'placeholder' => __('followup::lang.nots'),
-                                    'id' => 'nots',
+                                'class' => 'form-control',
+                                'placeholder' => __('followup::lang.nots'),
+                                'id' => 'nots',
                                 ]) !!}
                             </div>
                         </div>
@@ -77,17 +83,18 @@
 
 
 
-                    {{-- <div class="row" >
-                            <div class="col-md-12">
-                                <button type="submit"
-                                    class="btn btn-primary pull-right btn-flat ">@lang('messages.save')</button>
-                            </div>
-                        </div> --}}
+                    {{-- <div class="row">
+                        <div class="col-md-12">
+                            <button type="submit"
+                                class="btn btn-primary pull-right btn-flat ">@lang('messages.save')</button>
+                        </div>
+                    </div> --}}
 
 
                     <div class="modal-footer">
                         <button type="submit" class="btn btn-primary">@lang('messages.save')</button>
-                        <button type="button" class="btn btn-default" data-dismiss="modal">@lang('messages.close')</button>
+                        <button type="button" class="btn btn-default"
+                            data-dismiss="modal">@lang('messages.close')</button>
                     </div>
 
 
