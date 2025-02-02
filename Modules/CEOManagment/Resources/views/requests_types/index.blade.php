09bcfe824be6876c4ca450babee739da41fa0882
waleedcode <waleedflutter@gmail.com>
2025-02-02 10:29:49 +0300

    add permisstion

--- Modules/CEOManagment/Resources/views/requests_types/index.blade.php
+++ Modules/CEOManagment/Resources/views/requests_types/index.blade.php
@@ -48,7 +48,7 @@
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
 
-                        {!! Form::open(['route' => 'updateRequestType', 'enctype' => 'multipart/form-data']) !!}
+                        {!! Form::open(['route' => 'storeRequestType', 'enctype' => 'multipart/form-data']) !!}
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
                                     aria-hidden="true">&times;</span></button>
