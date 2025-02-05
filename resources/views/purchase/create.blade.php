593aa16ee279c166e9adc765f9ab1722a3b1e7e2
vic777or <victor.ahmad.mail@gmail.com>
2025-02-05 10:44:38 +0300

    Feat(Accounting): bread cramps part 3 Fix(Contact): add supplier

--- resources/views/purchase/create.blade.php
+++ resources/views/purchase/create.blade.php
@@ -8,6 +8,21 @@
     @endphp
     <!-- Content Header (Page header) -->
     <section class="content-header">
+        @if (isset($breadcrumbs))
+            <nav>
+                <ol class="breadcrumb">
+                    @foreach ($breadcrumbs as $breadcrumb)
+                        @if ($breadcrumb['url'])
+                            <li class="breadcrumb-item">
+                                <a href="{{ $breadcrumb['url'] }}">{{ $breadcrumb['title'] }}</a>
+                            </li>
+                        @else
+                            <li class="breadcrumb-item active">{{ $breadcrumb['title'] }}</li>
+                        @endif
+                    @endforeach
+                </ol>
+            </nav>
+        @endif
         <h1>@lang('purchase.add_purchase') <i class="fa fa-keyboard-o hover-q text-muted" aria-hidden="true" data-container="body"
                 data-toggle="popover" data-placement="bottom" data-content="@include('purchase.partials.keyboard_shortcuts_details')" data-html="true"
                 data-trigger="hover" data-original-title="" title=""></i></h1>
